cc26c19d1f12f01005ac8bdc60ef272a99342d6d
Jessica <fang.liu5@hp.com>
2016-02-17 17:09:09 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
+
 set -e
 
-# Set defaults
-: ${ELASTICSEARCH:="window.location.protocol + '//' + window.location.host"}
-: ${DEFAULT_ROUTE:="/dashboard/file/default.json"}
-
-# Check if we need quotes
-if [[ "$ELASTICSEARCH" != *\ * ]]; then
-    ELASTICSEARCH="'$ELASTICSEARCH'"
+# Add kibana as command if needed
+if [[ "$1" == -* ]]; then
+	set -- kibana "$@"
 fi
 
-# Update config
-sed -i -r -e "s|^(\s*elasticsearch)\s*:.*|\1: $ELASTICSEARCH,|" \
-          -e "s|^(\s*default_route)\s*:.*|\1: '$DEFAULT_ROUTE',|" \
-    /var/www/kibana/config.js
+# Run as user "kibana" if the command is "kibana"
+if [ "$1" = 'kibana' ]; then
+	if [ "$ELASTICSEARCH_URL" -o "$ELASTICSEARCH_PORT_9200_TCP" ]; then
+		: ${ELASTICSEARCH_URL:='http://elasticsearch:9200'}
+		sed -ri "s!^(\#\s*)?(elasticsearch_url:).*!\2 '$ELASTICSEARCH_URL'!" /opt/kibana/config/kibana.yml
+	else
+		echo >&2 'warning: missing ELASTICSEARCH_PORT_9200_TCP or ELASTICSEARCH_URL'
+		echo >&2 '  Did you forget to --link some-elasticsearch:elasticsearch'
+		echo >&2 '  or -e ELASTICSEARCH_URL=http://some-elasticsearch:9200 ?'
+		echo >&2
+	fi
+	
+	set -- gosu kibana tini -- "$@"
+fi
 
 exec "$@"
