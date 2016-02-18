e575aad05f3baae62007360b1cdaf0f2250f7f91
Jessica <fang.liu5@hp.com>
2016-02-18 15:30:39 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 set -e
 
-# Set defaults
-: ${ELASTICSEARCH:="window.location.protocol + '//' + window.location.host"}
-: ${DEFAULT_ROUTE:="/dashboard/file/default.json"}
-
-# Check if we need quotes
-if [[ "$ELASTICSEARCH" != *\ * ]]; then
-    ELASTICSEARCH="'$ELASTICSEARCH'"
-fi
-
-# Update config
-sed -i -r -e "s|^(\s*elasticsearch)\s*:.*|\1: $ELASTICSEARCH,|" \
-          -e "s|^(\s*default_route)\s*:.*|\1: '$DEFAULT_ROUTE',|" \
-    /opt/kibana/config.js
-
 exec "$@"
