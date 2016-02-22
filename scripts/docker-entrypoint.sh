5bc39a77ee20fc81c64082ca93baf79da1ec07ec
Jessica <fang.liu5@hp.com>
2016-02-22 16:25:40 +0800

    Update docker-entrypoint.sh

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
+set -e
 #change elasticsearch
 sed -ri "s!^(\#\s*)?(elasticsearch_url:).*!\2 '$ELASTICSEARCH_URL'!" /var/www/kibana/config/kibana.yml
 
 # start nginx
-service nginx start
+service nginx restart
+
+exec "$@"
